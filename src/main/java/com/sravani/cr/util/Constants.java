4997412b6fcb1b0764d8ad528b0d8d4c7b8d7332
gauravshilpakar <gaurav.shilpakar@gmail.com>
2023-04-25 13:48:31 -0400

    constants

---
+++ src/main/java/com/sravani/cr/util/Constants.java
@@ -0,0 +1,13 @@
+package com.sravani.cr.util;
+
+public class Constants {
+    public enum Roles{
+        ADMIN,
+        USER
+    }
+
+    public enum Authorities{
+        READ,
+        WRITE
+    }
+}
